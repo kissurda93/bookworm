f5b5098ef07c179134e6ea53e9d3f97de0f202da
kissurda93 <jg19930610@gmail.comgit config --global user.email jg19930610@gmail.comgit config --global user.email jg19930610@gmail.com>
2023-09-16 16:03:26 +0200

    Fixes: background and message popup

--- resources/js/Components/Message.jsx
+++ resources/js/Components/Message.jsx
@@ -1,27 +1,11 @@
 import "../../css/message.css";
-import { usePage } from "@inertiajs/inertia-react";
-import { useEffect } from "react";
 
-const Message = () => {
-  const { flash } = usePage().props;
-
-  useEffect(() => {
-    const messageTimeOut = setTimeout(() => {
-      flash.message = null;
-    }, 2000);
-
-    return () => {
-      clearTimeout(messageTimeOut);
-    };
-  }, [flash.message]);
-
+const Message = ({ message }) => {
   return (
     <>
-      {flash.message && (
-        <div className={flash.message.error ? "bad-message" : "good-message"}>
-          <p>{flash.message.text}</p>
-        </div>
-      )}
+      <div className={message?.error ? "bad-message" : "good-message"}>
+        <p>{message?.text}</p>
+      </div>
     </>
   );
 };
--- resources/js/Layout/Layout.jsx
+++ resources/js/Layout/Layout.jsx
@@ -2,21 +2,33 @@ import "../../css/layout.css";
 import "../Components/Navbar";
 import NavBar from "../Components/Navbar";
 import Message from "../Components/Message";
+import { usePage } from "@inertiajs/inertia-react";
+import { useEffect, useState } from "react";
 
 const Layout = ({ children }) => {
-  const currentUrl = window.location.href;
-  const welcomePage = import.meta.env.VITE_APP_URL;
+  const currentPath = window.location.pathname;
+  const [showMessage, setShowMessage] = useState(false);
+  const { flash } = usePage().props;
+
+  const hideMessage = () => setShowMessage(() => false);
+
+  useEffect(() => {
+    if (!showMessage && flash.message) {
+      setShowMessage(() => true);
+    }
+  }, [flash.message]);
 
   return (
     <div
+      onClick={() => hideMessage()}
       className={
-        currentUrl === welcomePage
+        currentPath === "/"
           ? "welcome-page-background-image"
           : "default-background-image"
       }
     >
       <NavBar />
-      <Message />
+      {showMessage && <Message message={flash.message} />}
       <div className="content">{children}</div>
     </div>
   );
